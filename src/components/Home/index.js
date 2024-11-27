2970c7dbe5f05155431eedae9bed1d5b194c1e6d
vik1234-del <victor.okolo@pau.edu.ng>
2024-11-27 13:05:29 +0100

    changes

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import AnimatedLetters from '../AnimatedLetters'
+
 import {
   faCss3,
   faGit,
@@ -8,7 +9,7 @@ import {
   faNodeJs,
   faReact,
 } from '@fortawesome/free-brands-svg-icons'
-import './index.scss'
+
 import Loader from 'react-loaders'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
@@ -17,8 +18,13 @@ import {
   faSquareXTwitter,
 } from '@fortawesome/free-brands-svg-icons'
 
+
+import './index.scss'
+
 const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
+  const [showContent, setShowContent] = useState(false)
+
   const nameArray = [
     ' ',
     'V',
@@ -36,10 +42,18 @@ const Home = () => {
   ]
 
   useEffect(() => {
-    const timeoutId = setTimeout(() => {
+    const letterAnimationTimeout = setTimeout(() => {
       setLetterClass('text-animate-hover')
     }, 4000)
-    return () => clearTimeout(timeoutId)
+
+    const contentTimeout = setTimeout(() => {
+      setShowContent(true)
+    }, 4000) 
+
+    return () => {
+      clearTimeout(letterAnimationTimeout)
+      clearTimeout(contentTimeout)
+    }
   }, [])
 
   return (
@@ -59,56 +73,63 @@ const Home = () => {
             />
           </h1>
           <h2>Full-Stack Developer | Video Editor | Entrepreneur</h2>
-
-          <div className="social-icons">
-            <a
-              href="https://www.linkedin.com/in/victor-okolo-b76203226/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FontAwesomeIcon icon={faLinkedin} className="social-icon" />
-            </a>
-            <a
-              href="https://twitter.com/vic_szn"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FontAwesomeIcon
-                icon={faSquareXTwitter}
-                className="social-icon"
-              />
-            </a>
-            <a
-              href="https://github.com/vik1234-del"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FontAwesomeIcon icon={faGithub} className="social-icon" />
-            </a>
-          </div>
+          {showContent && (
+            <>
+              <div className="social-icons">
+                <a
+                  href="https://www.linkedin.com/in/victor-okolo-b76203226/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FontAwesomeIcon icon={faLinkedin} className="social-icon" />
+                </a>
+                <a
+                  href="https://twitter.com/vic_szn"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FontAwesomeIcon
+                    icon={faSquareXTwitter}
+                    className="social-icon"
+                  />
+                </a>
+                <a
+                  href="https://github.com/vik1234-del"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FontAwesomeIcon icon={faGithub} className="social-icon" />
+                </a>
+              </div>
+            </>
+          )}
         </div>
-        <div className="stage-cube-cont">
-          <div style={{ marginTop: '-20px' }} className="cubespinner">
-            <div className="face1">
-              <FontAwesomeIcon icon={faNodeJs} color="#68A063" />
+        {showContent && (
+          <>
+            <div className="stage-cube-cont">
+              <div style={{ marginTop: '-20px' }} className="cubespinner">
+                <div className="face1">
+                  <FontAwesomeIcon icon={faNodeJs} color="#68A063" />
+                </div>
+                <div className="face2">
+                  <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+                </div>
+                <div className="face3">
+                  <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+                </div>
+                <div className="face4">
+                  <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+                </div>
+                <div className="face5">
+                  <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+                </div>
+                <div className="face6">
+                  <FontAwesomeIcon icon={faGit} color="#EC4D28" />
+                </div>
+              </div>
             </div>
-            <div className="face2">
-              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
-            </div>
-            <div className="face3">
-              <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
-            </div>
-            <div className="face4">
-              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
-            </div>
-            <div className="face5">
-              <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
-            </div>
-            <div className="face6">
-              <FontAwesomeIcon icon={faGit} color="#EC4D28" />
-            </div>
-          </div>
-        </div>
+          </>
+        )}
       </div>
 
       <Loader type="pacman" />
